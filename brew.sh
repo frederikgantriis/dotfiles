ead34db405d935634a7b4f65410c8e9f85684d4c
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-02-19 13:25:14 +0100

    feat: add brew install script

---
+++ brew.sh
@@ -0,0 +1,52 @@
+# Classic system packages
+brew install docker
+brew install dotnet
+brew install git
+
+# Improved version of vim
+brew install neovim
+
+# CLI for bitwarden
+brew install bitwarden-cli
+
+# Official CLI for git
+brew install gh
+
+# Better version of cat
+brew install bat
+
+# GUI for git
+brew install jesseduffield/lazygit/lazygit
+
+# Fix typos by writing "fuck" after mistyped command
+brew install thefuck
+
+# TLDR: Short explaination of command ex. "tldr git clone"
+brew install tldr
+
+# CLI for navigating across directories by using "z example-folder-name"
+brew install zoxide
+
+# Z-shell and all plugins used with it
+brew install zsh
+brew install zsh-autocomplete
+brew install zsh-autosuggestions
+brew install zsh-completions
+brew install zsh-fast-syntax-highlighting
+brew install zsh-syntax-highlighting
+
+# Other tools from brew leaves but not sure what it is
+brew install apktool
+brew install automake
+brew install cmake
+brew install dpkg
+brew install git-lfs
+brew install go
+brew install jpeg
+brew install make
+brew install mozjpeg
+brew install nasm
+brew install nmap
+brew install pkg-config
+brew install postgis
+brew install yakitrak/yakitrak/obs
