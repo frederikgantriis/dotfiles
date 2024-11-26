613a840f5a9c88c41e5b91c0d53369a99221cfcc
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-11-26 08:20:20 +0100

    minor: update lazyvim and change LetItSnow to start on `BufEnter`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     vim.cmd("LetItSnow")
   end,
