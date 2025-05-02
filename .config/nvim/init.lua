70ea700ee9aca9a5ba21bcc6a54fef4e7179a24f
Frederik Gantriis Møller <frmo24042@gmail.com>
2025-05-02 10:43:58 +0200

    chore: update packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
-  callback = function()
-    vim.cmd("LetItSnow")
-  end,
-})
